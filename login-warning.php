44d636ab56610f2056f9ddfc847bcea3bc13b751
chuckingit <chuck@avantigroup.com>
2014-01-10 19:04:55 -0500

    added meta redirect and error msg
    i added a meta redirect of 6 seconds before the page bounces back to a home page (change domain.com) ... also tweaked the error message to show that location and/or device triggered the bounce

--- login-warning.php
+++ login-warning.php
@@ -1,21 +1,27 @@
 <!DOCTYPE>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
-<head profile="http://gmpg.org/xfn/11">
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Login Warning</title>
-	<meta name="robots" content="noodp,noydir" />
-	<style>
-	#wrapper {
-		width: 960px;
-		margin: 20px auto;
-	}
-	</style>
-
+<head>
+<title>Sorry - Log In Warning</title>
+<meta http-equiv="refresh" content="6; url=http://www.mydomain.com/" />	
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="cache-control" content="no-cache" />
+<meta name="robots" content="noindex,nofollow" />
+<style>
+#wrapper {
+	margin-top: 40px;
+	padding: 20px;
+	height: 300px;
+	width: 500px;
+	background: #eee;
+	margin: 0 auto; 
+}
+</style>
 </head>
 <body>
 	<div id="wrapper">
-	<h1>Login Warning</h1>
-	<p>There was an issue with your log in. Your user account has logged in recently from a different location.</p>
+		<h1>Sorry - Log In Session Expired</h1>
+		<p>Note: This account has recently logged in from a different location and/or device.</p>
+		<p>Hence this session is expired (e.g., only one log in per account).</p>
 	</div>
 </body>
 </html>
